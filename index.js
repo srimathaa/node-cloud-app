cb10d2f0a74f4adcf7e0281c4f9605c0a846f661
Srimatha25 <srimathasrimi85@gmail.com>
2026-01-28 19:52:06 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`
+    <h1>Hello from the Cloud ☁️</h1>
+    <p>Server running on Node.js</p>
+    <p>Time: ${new Date()}</p>
+  `);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
